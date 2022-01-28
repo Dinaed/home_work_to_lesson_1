cfc3565275c6846deda6be23bfd6428e89481dae
Dinaed <Dinaedandr@gmail.com>
2022-01-28 19:10:01 +0200

    final

---
+++ app.ts
@@ -0,0 +1,12 @@
+namespace Drive {
+  export class Cars {
+   
+      constructor(public name: string){
+      }
+  }
+  export class Motorbike {
+   
+      constructor(public name: string){
+      }
+  }
+}
